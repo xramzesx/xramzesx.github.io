8fee825cee4e508af7ce68ebc7df182d7fb288ad
xramzesx <xramzesbusiness@gmail.com>
2026-03-19 23:39:22 +0100

    Add sizes.ts

---
+++ src/constants/sizes.ts
@@ -0,0 +1,24 @@
+const common = {
+  small: 8,
+  normal: 16,
+  large: 24,
+  xlarge: 32,
+  xxlarge: 48,
+};
+
+const font = {
+  small: common.small,
+  normal: common.normal,
+};
+
+const safeArea = {
+  wide: 1920,
+  normal: 1200,
+};
+
+const screen = {
+  desktop: "1200px",
+  mobile: "720px",
+};
+
+export default { screen, safeArea };
