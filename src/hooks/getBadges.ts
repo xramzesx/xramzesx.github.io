d4b69cb079ccd32444a0cbff7fedc22599260a84
xramzesx <xramzesbusiness@gmail.com>
2026-03-26 08:28:46 +0100

    Add getBadges and useSkills

---
+++ src/hooks/getBadges.ts
@@ -0,0 +1,30 @@
+import iconsData from "@/assets/data/icons.json";
+
+const iconsMap = new Map<string, IconData>(
+  (iconsData as IconData[]).map((icon) => [icon.iconId, icon]),
+);
+
+const getIconSources = (iconId: IconId) => {
+  const prefix = `icons/${iconId}`;
+  return {
+    idleSrc: `${prefix}-idle.svg`,
+    activeSrc: `${prefix}-active.svg`,
+  };
+};
+
+const getBadges = (badges: BadgeData[]): ResolvedBadge[] =>
+  badges
+    .map(({ iconId, href }) => {
+      const icon = iconsMap.get(iconId)!;
+      return {
+        icon: {
+          alt: icon.name,
+          ...icon,
+          ...getIconSources(iconId),
+        },
+        href,
+      };
+    })
+    .filter(Boolean);
+
+export { getBadges };
